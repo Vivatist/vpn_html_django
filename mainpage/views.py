909114a462fdda9d4bdf3f17321db1c7bcfa1ba9
Vivatist <andrey.bch.1976@gmail.com>
2023-09-22 20:31:55 +0300

    Шаблон работает с базой

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from mainpage.links import Links
-from .models import *
+from .models import Links
 
 
 def get_client_ip(req):
@@ -14,19 +13,21 @@ def get_client_ip(req):
 
 
 def index(request):
-    class Settings:
-        url = "http://sssvpn.ru"
-        url_support = "https://t.me/+hU9vIxKbtwg3ZGI6"
-        # host = "127.0.0.1"
-        host = "5.104.108.237"
-        port = "14983"
-        password = "238938"
-        encription = "AES-256-GCM"
-        ss_link = "ss://YWVzLTI1Ni1nY206MjM4OTM4@5.104.108.237:14983/#sssvpn.ru"
-        links = Links.objects
+    context = {
+        "url": "http://sssvpn.ru",
+        "url_support": "https://t.me/+hU9vIxKbtwg3ZGI6",
+        "host": "5.104.108.237",
+        "port": "14983",
+        "password": "238938",
+        "encription": "AES-256-GCM",
+        "ss_link": "ss://YWVzLTI1Ni1nY206MjM4OTM4@5.104.108.237:14983/#sssvpn.ru",
+        "android_client": Links.objects.get(name="android_client").url,
+        "windows_client": Links.objects.get(name="windows_client").url,
+        "ios_client": Links.objects.get(name="ios_client").url,
+    }
 
     ip_addr = get_client_ip(request)
-    check = ip_addr == Settings.host
+    check = ip_addr == context["host"]
     print("IP:", ip_addr, " Check:", check)
 
-    return render(request, "index.html", {"settings": Settings, "links": Links})
+    return render(request, "mainpage/index.html", context=context)
