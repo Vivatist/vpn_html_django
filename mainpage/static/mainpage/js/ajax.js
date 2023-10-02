5ba8227c34347637c3600694ad2ef33bf7dcf552
Vivatist <anvabo@gmail.com>
2023-10-02 10:26:21 +0300

    ajax в отдельный js

---
+++ mainpage/static/mainpage/js/ajax.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    setInterval(function ()  {
+        // AJAX - запрашивает у сервера обновленный ip клиента раз в 2 секунды
+        $.ajax({
+            url: '/update_ip/',
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                // Обработка полученных данных
+                $('#ip').text("Ваш IP: " + data.ip);
+                var x = data.check_ip
+                html_warning = 'Статус подключения: <span class="badge rounded-pill bg-warning text-dark">НЕ ПОДКЛЮЧЕН</span>. Сначала подключитесь к VPN, а затем попробуйте открыть заблокированные Роскомнадзором ресурсы:'
+                html_success = 'Статус подключения: <span class="badge rounded-pill bg-success">ПОДКЛЮЧЕН</span>. Теперь вы можете открывать заблокированные Роскомнадзором ресурсы. Помните, если какой-то сайт не открывается, это еще не значит что проблема в VPN - в наше время все возможно, пробуйте открыть другие ресурсы.'
+                if (x) {
+                  $('#test').html(html_success)
+                  $("#tablet").css("display", "block"); 
+                } else {
+                  $('#test').html(html_warning)
+                  $("#tablet").css("display", "none"); 
+                }    
+                
+            }
+        });
+    }, 2000);
+    });
